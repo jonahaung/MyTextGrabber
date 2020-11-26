5f62f2d5aeb16c947f2fcc73ced26b73ad34c540
aungkomin <jonahaung@gmail.com>
2020-11-26 11:31:41 +0800

    Initial Commit

---
+++ MyTextGrabber/MyTextGrabberApp.swift
@@ -0,0 +1,20 @@
+//
+//  MyTextGrabberApp.swift
+//  MyTextGrabber
+//
+//  Created by Aung Ko Min on 26/11/20.
+//
+
+import SwiftUI
+
+@main
+struct MyTextGrabberApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
